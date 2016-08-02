993e5dfd6dbd86761e4ab8a2b253777c8497ca6d
Jarryd Pearson <jp@ssby.me>
2016-08-02 21:58:47 +1000

    / ‘test3.php’

--- test3.php
+++ test3.php
@@ -2,22 +2,25 @@
     $password = $_POST['Password'];
     $password = md5(sha1($password));
 
-    if( empty($_POST) ) {
-        echo '<form action="" method="post">
-            <input type="password" name="Password" />
-            <input type="submit" value="Submit" />
-        </form>';
+    // if( empty($_POST) ) {
+    //     echo '<form action="" method="post">
+    //         <input type="password" name="Password" />
+    //         <input type="submit" value="Submit" />
+    //     </form>';
+    // }
+    // elseif( !empty($_POST) && $password != 'cd8877aef9f02a65df87c06204d6ad0f' ) {
+    //     echo '<p style="color: red;">
+    //     Password is wrong.
+    //     </p>
+    //     <form action="" method="post">
+    //         <input type="password" name="Password" />
+    //         <input type="submit" value="Submit" />
+    //     </form>';
+    // }
+    if( empty($_GET) ) {
+
     }
-    elseif( !empty($_POST) && $password != 'cd8877aef9f02a65df87c06204d6ad0f' ) {
-        echo '<p style="color: red;">
-        Password is wrong.
-        </p>
-        <form action="" method="post">
-            <input type="password" name="Password" />
-            <input type="submit" value="Submit" />
-        </form>';
-    }
-    else {
+    elseif( $_GET['show'] == 'true' ) {
     //echo md5(sha1('FireSword$06'));
 ?><html>
 <head>

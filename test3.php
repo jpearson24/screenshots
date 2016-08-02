ad745fcddb9e130e7767e00c719c881733e5fd3f
Jarryd Pearson <jp@ssby.me>
2016-08-02 21:09:49 +1000

    / ‘test3.php’

--- test3.php
+++ test3.php
@@ -16,7 +16,7 @@
         //     //position: 'left'
         // });
         $(document).ready(function() {
-            e.preventDefault();
+            $(".livepreview").click(preventDefault());
             $(".livepreview").click(function() {
                 $.ajax({
                     url : $(this).attr("href"),

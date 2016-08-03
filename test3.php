53993c908d98410a37db51a84fa900c95fbb3d52
Jarryd Pearson <jp@ssby.me>
2016-08-03 20:28:45 +1000

    / ‘test3.php’

--- test3.php
+++ test3.php
@@ -2,21 +2,21 @@
     $password = $_POST['Password'];
     $password = md5(sha1($password));
 
-    // if( empty($_POST) ) {
-    //     echo '<form action="" method="post">
-    //         <input type="password" name="Password" />
-    //         <input type="submit" value="Submit" />
-    //     </form>';
-    // }
-    // elseif( !empty($_POST) && $password != 'cd8877aef9f02a65df87c06204d6ad0f' ) {
-    //     echo '<p style="color: red;">
-    //     Password is wrong.
-    //     </p>
-    //     <form action="" method="post">
-    //         <input type="password" name="Password" />
-    //         <input type="submit" value="Submit" />
-    //     </form>';
-    // }
+    if( empty($_POST) ) {
+        echo '<form action="" method="post">
+            <input type="password" name="Password" />
+            <input type="submit" value="Submit" />
+        </form>';
+    }
+    elseif( !empty($_POST) && $password != 'cd8877aef9f02a65df87c06204d6ad0f' ) {
+        echo '<p style="color: red;">
+        Password is wrong.
+        </p>
+        <form action="" method="post">
+            <input type="password" name="Password" />
+            <input type="submit" value="Submit" />
+        </form>';
+    }
     if( empty($_GET) ) {
 
     }

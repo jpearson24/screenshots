4bd649e4d07d828d6b8591061e4eee972f7b3d98
Jarryd Pearson <jp@ssby.me>
2016-08-04 11:32:06 +1000

    / ‘process.php’

--- process.php
+++ process.php
@@ -2,7 +2,7 @@
     $password = $_POST['Password'];
     $logout = $_POST['logout'];
     // Hash: cd8877aef9f02a65df87c06204d6ad0f
-    echo md5(sha1('password'));
+    //echo md5(sha1(''));
 
     if( $logout ) {
         unset($_COOKIE['login']);
@@ -14,10 +14,10 @@
     }
     elseif( isset($password) ) {
         $password = md5(sha1($password));
-        if( $password != 'cd8877aef9f02a65df87c06204d6ad0f' ) {
+        if( $password != 'cd8877aef9f02a65df87c06204d6ad0f' || $password != '1619d7adc23f4f633f11014d2f22b7d8' ) {
             header('Location: https://ssby.me/ss/?p=w');
         }
-        else {
+        elseif( $password == '1619d7adc23f4f633f11014d2f22b7d8' || $password != 'cd8877aef9f02a65df87c06204d6ad0f' ) {
             setcookie('login', 'allow', time()+60*60*24*365, '/ss');
             header('Location: https://ssby.me/ss/');
         }

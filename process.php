64835ebedf51d5536a486b0b73f994838517ceb8
Jarryd Pearson <jp@ssby.me>
2016-08-04 11:41:12 +1000

    / ‘process.php’

--- process.php
+++ process.php
@@ -17,7 +17,7 @@
         if( $password != 'cd8877aef9f02a65df87c06204d6ad0f' || $password != '1619d7adc23f4f633f11014d2f22b7d8' ) {
             header('Location: https://ssby.me/ss/?p=w');
         }
-        elseif( $password == '1619d7adc23f4f633f11014d2f22b7d8' || $password != 'cd8877aef9f02a65df87c06204d6ad0f' ) {
+        elseif( $password == '1619d7adc23f4f633f11014d2f22b7d8' || $password == 'cd8877aef9f02a65df87c06204d6ad0f' ) {
             setcookie('login', 'allow', time()+60*60*24*365, '/ss');
             header('Location: https://ssby.me/ss/');
         }
